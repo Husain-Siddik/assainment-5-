dfdca8898faba97a9b6596225b2d5c96b4bf6638
Husain-Siddik <ronysiddik21@gmail.com>
2024-02-21 22:01:09 +0600

    append element done

--- js/first.js
+++ js/first.js
@@ -14,22 +14,33 @@ let totalprice = 0
 for (const seatButon of allSeatButton) {
     //console.log(seatButon);
 
-    seatButon.addEventListener('click', function (e) {
+    seatButon.addEventListener('click', function (event) {
 
-
-        // total seat bokking
-        //4 ter besi seat kata jabe na
-        const tergetText = e.target.parentNode.childNodes[1].innerText;
-         console.log(tergetText);
-
-
-        //this.disabled.classList.add('bg-green-500') 
         seatButon.classList.add("bg-green-500")
 
         if (count <= 3) {
             count = count + 1;
             // appending alliment
-
+            const name = event.target.parentNode.childNodes[1].innerText;
+            const category = "Economoy";
+            const price = 550;
+            const selectedContainer = document.getElementById('book-seat');
+    
+            const div = document.createElement("div");
+            div.classList.add("flex");
+            div.classList.add("justify-between");
+            div.classList.add("py-3");
+            div.classList.add('text-xl')
+            const p1 = document.createElement("p");
+            const p2 = document.createElement("p");
+            const p3 = document.createElement("p");
+            p1.innerText = name;
+            p2.innerText = category;
+            p3.innerText = price;
+            div.appendChild(p1);
+            div.appendChild(p2);
+            div.appendChild(p3);
+            selectedContainer.appendChild(div);
             //appent element finished
             totalprice = parseInt(totalprice + 550);
             //total price 
@@ -60,19 +71,19 @@ document.getElementById('apply').addEventListener('click', function () {
     this.disabled = true;
     const cuponField = document.getElementById('cupun-text').value;
     if (cuponField === "Couple20") {
-        discount ('grand-total' ,20);
+        discount('grand-total', 20);
         const inputField = document.getElementById('cupun-text');
         inputField.classList.add('hidden');
         // modal re vai  //
         const showModal = document.getElementById('next-btn');
         showModal.addEventListener('click', function () {
             my_modal_5.showModal();
-            console.log("modal ");
+            // console.log("modal ");
         })
 
     }
     else if (cuponField === "NEW15") {
-        discount ('grand-total',15);
+        discount('grand-total', 15);
         const inputField = document.getElementById('cupun-text');
         inputField.classList.add('hidden')
     }
@@ -86,7 +97,7 @@ document.getElementById('apply').addEventListener('click', function () {
 
 //  functions
 
-function discount (id ,n){
+function discount(id, n) {
     const grandTotal = document.getElementById(id);
     const grandTotalValue = parseInt(grandTotal.innerText);
     grandTotal.innerText = grandTotalValue - (grandTotalValue * n / 100);
